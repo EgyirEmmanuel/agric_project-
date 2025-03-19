ac9ac0356ec2e143f7f94ccea299b119ab7d1b83
Emmnauel Egyir <egyiremmanuel380@gmail.com>
2025-03-19 09:02:33 +0000

    footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,18 +5,17 @@ const Footer = () => {
   return (
     <footer className="bg-bgBody text-white font-poppins font-normal text-[15px]">
       <div className="max-w-6xl mx-auto px-4 py-8">
-       
-      
 
         {/* Bottom section with links and copyright */}
         <div className="pt-4 flex flex-col md:flex-row justify-between items-center ">
-          <div className="flex flex-wrap gap-5 mb-3 md:mb-0 text-sm text-white">
+          <div className="flex flex-wrap  justify-center text-center  gap-5 mb-3 md:mb-0 text-sm text-white">
             <Link href="/about" className="hover:text-hoverColor transition duration-300">About us</Link>
             <Link href="#" className="hover:text-hoverColor transition duration-300">Contact</Link>
             <Link href="#" className="hover:text-hoverColor transition duration-300">Privacy policy</Link>
             <Link href="#" className="hover:text-hoverColor transition duration-300">Sitemap</Link>
 
           </div>
+          
           <div className="text-white font-poppins font-normal text-[15px]">
             © 2025, All Rights Reserved
           </div>
--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,57 +4,65 @@ import { useState } from "react";
 import Link from "next/link";
 import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 
-
 import { SignInButton, SignedOut, SignedIn } from "@clerk/nextjs";
 
-
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleClick = () => {
-      setIsOpen(!isOpen);
-    };
+  const handleMobileMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div className="w-full h-20 bg-bgBody border-b-[1px] border-bgButton">
-    <div className="w-full bg-bgBody px-10 md:px-20 mx-auto h-full max-w-screen-2xl flex items-center justify-between">
-         {/* LOGO */}
-         <Link href='/' className={`text-2xl text-white font-semibold font-poppins cursor-pointer`}>
-         GAIA Club
-         </Link>
-        
-          {/* DESKTOP NAVBAR */}
-         <div className="hidden md:block ">
+      <div className="w-full bg-bgBody px-10 md:px-20 mx-auto h-full max-w-screen-2xl flex items-center justify-between">
+        {/* Logo */}
+        <Link
+          href="/"
+          className={`text-2xl text-white font-semibold font-poppins cursor-pointer`}
+        >
+          GAIA Club
+        </Link>
+
+        {/* DESKTOP NAVBAR */}
+        <div className="hidden md:block ">
           <div className="flex items-center justify-between space-x-10">
-          <ul className="flex space-x-10 text-white  font-poppins font-normal text-[15px]">
-            <li className="hover:text-hoverColor transition-colors duration-100">
-              <Link href="/about">About Us</Link>
-            </li>
-            <li className="hover:text-hoverColor transition-colors duration-100">
-              <Link href="#">Technology</Link>
-            </li>
-            <li className="hover:text-hoverColor transition-colors duration-100">
-              <Link href="#">Contact</Link>
-            </li> 
-          </ul>
+            <ul className="flex space-x-10 text-white  font-poppins font-normal text-[15px]">
+              <li className="hover:text-hoverColor transition-colors duration-100">
+                <Link href="/about">About Us</Link>
+              </li>
+              <li className="hover:text-hoverColor transition-colors duration-100">
+                <Link href="#">Technology</Link>
+              </li>
+              <li className="hover:text-hoverColor transition-colors duration-100">
+                <Link href="#">Contact</Link>
+              </li>
+            </ul>
 
-          {/* Sign in btn */}
-          <SignedOut >
-            <SignInButton>
-              <button  className="bg-bgButton border-none p-2 text-bgBody rounded-full font-poppins font-semibold">Sign in</button>
-            </SignInButton>
-          </SignedOut>
+            {/* Sign in btn */}
+            <SignedOut>
+              <SignInButton>
+                <button className="bg-bgButton border-none p-2 text-bgBody rounded-full font-poppins font-semibold">
+                  Sign in
+                </button>
+              </SignInButton>
+            </SignedOut>
 
-          {/* Dashboard btn */}
-          <SignedIn>
-            <Link
-              href="/dashboard"
-              className="bg-bgButton p-2 rounded-full hover:bg-opacity-80 transition-opacity duration-100"
-            >
-             <button className=" border-none p- text-bgBody rounded-full font-poppins font-semibold">Dashboard</button>
-            </Link>
-          </SignedIn>
+            {/* Dashboard btn */}
+            <SignedIn>
+              <Link
+                href="/dashboard"
+                className="bg-bgButton p-2 rounded-full hover:bg-opacity-80 transition-opacity duration-100"
+              >
+                <button className=" border-none p- text-bgBody rounded-full font-poppins font-semibold">
+                  Dashboard
+                </button>
+              </Link>
+            </SignedIn>
           </div>
         </div>
 
@@ -66,8 +74,8 @@ const Header = () => {
           <RxHamburgerMenu />
         </span>
 
-         {/* MOBILE NAVBAR */}
-         {isOpen && (
+        {/* MOBILE NAVBAR */}
+        {isOpen && (
           <div className=" md:hidden w-full fixed inset-0 bg-bgBody z-50 ">
             <div className="w-full h-full flex flex-col items-center justify-center relative">
               <span
@@ -77,39 +85,54 @@ const Header = () => {
                 <RxCross2 />
               </span>
               <ul className="flex flex-col space-y-5 text-white font-normal text-center justify-center text-2xl font-poppins">
-                <li className="hover:text-hoverColor transition-colors duration-100">
+                <li
+                  className="hover:text-hoverColor transition-colors duration-100"
+                  onClick={handleMobileMenu}
+                >
                   <Link href="/about">About Us</Link>
                 </li>
-                <li className="hover:text-hoverColor transition-colors duration-100">
+                <li
+                  className="hover:text-hoverColor transition-colors duration-100"
+                  onClick={handleMobileMenu}
+                >
                   <Link href="#">Technology</Link>
                 </li>
-                <li className="hover:text-hoverColor transition-colors duration-100">
+                <li
+                  className="hover:text-hoverColor transition-colors duration-100"
+                  onClick={handleMobileMenu}
+                >
                   <Link href="#">Contact</Link>
                 </li>
-                  <li>
-                    <SignedIn>
+                <li>
+                  <SignedIn>
                     <Link
-                    href="/dashboard"
-                    className="bg-bgButton p-2 text-bgBody font-semibold rounded-full  hover:bg-opacity-80 transition-opacity duration-100"
-                  >
-                    Dashboard
-                  </Link>
+                      href="/dashboard"
+                      className="bg-bgButton p-2 text-bgBody font-semibold rounded-full  hover:bg-opacity-80 transition-opacity duration-100"
+                      onClick={handleMobileMenu}
+                    >
+                      Dashboard
+                    </Link>
                   </SignedIn>
                 </li>
                 <li>
-                <SignedOut >
-            <SignInButton>
-              <button  className="bg-bgButton border-none p-2 text-bgBody rounded-full font-poppins font-semibold">Sign in</button>
-            </SignInButton>
-          </SignedOut>
+                  <SignedOut>
+                    <SignInButton>
+                      <button
+                        className="bg-bgButton border-none p-2 text-bgBody rounded-full font-poppins font-semibold"
+                        onClick={handleMobileMenu}
+                      >
+                        Sign in
+                      </button>
+                    </SignInButton>
+                  </SignedOut>
                 </li>
               </ul>
             </div>
           </div>
         )}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
